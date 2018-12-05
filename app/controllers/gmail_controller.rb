a3d0bbeddac0e00b488d72ba2426fcd74f892284
日野恵太 <keitahino0415@gmail.com>
2018-12-05 18:01:58 +0900

    feat:notifyアクション追加

--- app/controllers/gmail_controller.rb
+++ app/controllers/gmail_controller.rb
@@ -1,8 +1,13 @@
 class GmailController < ApplicationController
   protect_from_forgery :except => [:callback]
+  CITY_NAME = 'ishinomaki'
   def callback
-    city_name = "ishinomaki"
-    recorder = Recorder.new(city_name)
+    recorder = Recorder.new(CITY_NAME)
     recorder.insert_weather_log
+  end
+
+  def notify
+    recorder = Recorder.new(CITY_NAME)
+    recorder.today_weather
   end
 end
