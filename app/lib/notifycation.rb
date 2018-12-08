c996bd9a84bf39cd280a18029c4696dcdd849cc8
日野恵太 <keitahino0415@gmail.com>
2018-12-08 11:42:24 +0900

    refactor:メッセージを生成する処理とlineにpushする処理を分離

--- app/lib/notifycation.rb
+++ app/lib/notifycation.rb
@@ -1,24 +1,27 @@
 class Notifycation
 
   def line_notify(city_name)
-    recorder = Recorder.new(city_name)
-    @delay_date = recorder.threshold_over_list
     request = Request.new(line_endpoint)
     authorization = "Bearer #{line_accsess_key}"
-    request.post(authorization,message)
+    request.post(authorization,message(city_name))
+  end
+
+  def message(city_name)
+    message = []
+    message << "\n本日、下記の時間帯にJRが遅延する恐れがあります"
+    recorder = Recorder.new(city_name)
+    eva_wind = recorder.wind_speed_average
+
+    recorder.weather_today.each do |list|
+      if eva_wind <= list['wind']['speed'] then
+        message.push(list['dt_txt'] + "\n")
+      end
+    end
+    return message.join
   end
 
   def line_accsess_key
     line_accsess_key = ENV['LINE_NOTIFY_KEY']
-  end
-
-  def message
-    buf = []
-    buf << "\n本日、下記の時間帯にJRが遅延する恐れがあります"
-    @delay_date.each do |msg|
-      buf << msg + "\n"
-    end
-    buf.join
   end
 
   def line_endpoint
