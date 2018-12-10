065152ddc128deae99f801794c05d65977f3ca51
日野恵太 <keitahino0415@gmail.com>
2018-12-10 13:54:46 +0900

    add:threshold_latestメソッド追加

--- app/models/threshold.rb
+++ app/models/threshold.rb
@@ -1,2 +1,8 @@
 class Threshold < ApplicationRecord
+  def self.threshold_latest(city_name)
+    self.where(city_name:"ishinomaki")
+        .pluck(:wind_speed)
+        .last.to_f
+        .round(2)
+  end
 end
