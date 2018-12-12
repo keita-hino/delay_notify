c14b3739d8f183a76bd451278860a57af0ac22ec
日野恵太 <keitahino0415@gmail.com>
2018-12-12 14:55:59 +0900

    fix:遅延がない場合は通知を出さないように修正

--- app/lib/notifycation.rb
+++ app/lib/notifycation.rb
@@ -3,30 +3,37 @@ class Notifycation
   def line_notify(city_name)
     request = Request.new(line_endpoint)
     authorization = "Bearer #{line_accsess_key}"
-    request.post(message(city_name),authorization)
+    if message = create_message(city_name)
+      request.post(message,authorization)
+    end
   end
 
   def slack_notify(city_name)
     request = Request.new(slack_endpoint)
-    param = {
-              token:ENV['SLACK_BOT_OAUTH'],
-              channel: "#train-info",
-              text: message(city_name),
-              as_user: true,
-            }
-    request.slack_post(param)
+    if message = create_message(city_name)
+      param = {
+                token:ENV['SLACK_BOT_OAUTH'],
+                channel: "#train-info",
+                text: message,
+                as_user: true,
+              }
+      request.slack_post(param)
+    end
   end
 
-  def message(city_name)
+  def create_message(city_name)
     message  = "\n本日から5日間の範囲で、下記の時間帯にJRが遅延する恐れがあります\n"
     recorder = Recorder.new(city_name)
     eva_wind = Threshold.threshold_latest(city_name)
+    count = 0
 
     recorder.weather_today.each do |list|
       if eva_wind <= list['wind']['speed'] then
         message << list['dt_txt'] + "\n"
+        count += 1
       end
     end
+    message = nil if count == 0
     return message
   end
 
