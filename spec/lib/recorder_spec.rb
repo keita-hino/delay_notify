b7a278998cb25362eeb80467b735a9bf9669bd51
日野恵太 <keitahino0415@gmail.com>
2018-12-15 21:51:29 +0900

    refactor:テストを修正

--- spec/lib/recorder_spec.rb
+++ spec/lib/recorder_spec.rb
@@ -15,43 +15,43 @@ describe Recorder do
     end
 
   end
-  describe 'direction convert' do
-    it 'direction convert to north' do
+  context 'direction convert' do
+    it 'return north' do
       expect(@recorder.direction(0)).to eq("北")
       expect(@recorder.direction(39)).to eq("北")
     end
 
-    it 'direction convert to northeast' do
+    it 'return northeast' do
       expect(@recorder.direction(40)).to eq("北東")
       expect(@recorder.direction(89)).to eq("北東")
     end
 
-    it 'direction convert to east' do
+    it 'return east' do
       expect(@recorder.direction(90)).to eq("東")
       expect(@recorder.direction(139)).to eq("東")
     end
 
-    it 'direction convert to southeast' do
+    it 'return southeast' do
       expect(@recorder.direction(140)).to eq("南東")
       expect(@recorder.direction(179)).to eq("南東")
     end
 
-    it 'direction convert to south' do
+    it 'return south' do
       expect(@recorder.direction(180)).to eq("南")
       expect(@recorder.direction(219)).to eq("南")
     end
 
-    it 'direction convert to southwest' do
+    it 'return southwest' do
       expect(@recorder.direction(220)).to eq("南西")
       expect(@recorder.direction(269)).to eq("南西")
     end
 
-    it 'direction convert to west' do
+    it 'return west' do
       expect(@recorder.direction(270)).to eq("西")
       expect(@recorder.direction(309)).to eq("西")
     end
 
-    it 'direction convert to northsouth' do
+    it 'return northsouth' do
       expect(@recorder.direction(310)).to eq("北西")
       expect(@recorder.direction(360)).to eq("北西")
     end
