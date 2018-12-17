8ce1f11b5e7093b4f39876547f0bc2b58546a7f5
日野恵太 <keitahino0415@gmail.com>
2018-12-17 16:32:50 +0900

    refactor:テスト修正

--- spec/controllers/gmail_controller_spec.rb
+++ spec/controllers/gmail_controller_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 
 RSpec.describe GmailController, type: :controller do
   describe 'POST #callback' do
-    it 'callback' do
+    it 'saves the new weather and threshold in the database' do
       post :callback,params: { test: "test" }
       expect(response).to have_http_status(:success)
     end
   end
   describe 'GET #notify' do
-    it 'notify' do
+    it 'notify via line and slack' do
       get :notify
       expect(response).to have_http_status(:success)
     end
