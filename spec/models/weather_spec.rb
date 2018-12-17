49d34c830245e8eb8aa9741c11ae96738ad665ec
日野恵太 <keitahino0415@gmail.com>
2018-12-17 12:58:42 +0900

    add:wind_speed_averageメソッドのテスト追加

---
+++ spec/models/weather_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe Weather, type: :model do
+  before do
+    FactoryBot.create(:weather,wind_speed:1)
+    FactoryBot.create(:weather,wind_speed:3)
+  end
+  it 'is average value correct' do
+    expect(Weather.wind_speed_average("ishinomaki")).to eq(2)
+  end
+end
