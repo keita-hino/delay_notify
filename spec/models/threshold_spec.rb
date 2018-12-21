3b6adb8989811681149bd0207c4e39a8cdac5b2a
日野恵太 <keitahino0415@gmail.com>
2018-12-21 13:02:10 +0900

    add:threshold_updateのテストケース追加

--- spec/models/threshold_spec.rb
+++ spec/models/threshold_spec.rb
@@ -2,11 +2,21 @@ require 'rails_helper'
 
 RSpec.describe Threshold, type: :model do
   before do
+    FactoryBot.create(:weather,wind_speed:3)
+    FactoryBot.create(:weather,wind_speed:5)
     FactoryBot.create(:threshold)
   end
   describe '#threshold_latest' do
     it 'is threshold value correct' do
       expect(Threshold.threshold_latest('ishinomaki')).to eq(16)
+    end
+  end
+  describe '#threshold_update' do
+    it 'is threshold update result correct' do
+      Threshold.threshold_update("ishinomaki")
+      expect(Threshold.where(city_name:'ishinomaki')
+                      .pluck(:wind_speed)
+                      .last).to eq(4)
     end
   end
 end
