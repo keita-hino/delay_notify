ba9543bfca20c0014143e29cc766ff0a06a3a988
日野恵太 <keitahino0415@gmail.com>
2018-12-21 13:01:27 +0900

    add:threshold_updateメソッドを追加

--- app/models/threshold.rb
+++ app/models/threshold.rb
@@ -1,8 +1,22 @@
 class Threshold < ApplicationRecord
   def self.threshold_latest(city_name)
-    self.where(city_name:"ishinomaki")
+    self.where(city_name:city_name)
         .pluck(:wind_speed)
-        .last.to_f
+        .last
+        .to_f
         .round(2)
   end
+
+  def self.threshold_update(city_name)
+    result = Weather.where(city_name:city_name)
+                    .average(:wind_speed)
+                    .to_f
+                    .round(2)
+
+    self.create(
+      wind_speed:result,
+      city_name:city_name
+    )
+  end
+
 end
